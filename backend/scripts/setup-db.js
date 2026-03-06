d7f9cb43108d6998a59e064c0bdfb20fab63d60e
Rin <aporia1203@gmail.com>
2026-03-06 17:54:50 +0800

    Initial commit

--- backend/scripts/setup-db.js
+++ backend/scripts/setup-db.js
@@ -7,7 +7,12 @@ const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: 'crossover.proxy.rlwy.net',
+  port: 28569,
+  user: 'postgres',
+  password: 'DNahkkYnaMcFrByDrocsgZARqfERYkoq',
+  database: 'railway',
+  ssl: { rejectUnauthorized: false },
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
